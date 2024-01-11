29c5efa60ac6da7242ad24a5a9a207d2308505d1
kyungjae-Y <yoonkj2008@gmail.com>
2024-01-11 14:54:04 +0900

    Create Ex08_hoisting.js

---
+++ javascript/Ex08_hoisting.js
@@ -0,0 +1,17 @@
+console.log(name);
+var name = '윤경재';
+console.log(name);
+
+function sayHi() {
+  console.log('hello');
+}
+sayHi()
+
+sayHi()
+
+function sayHi() {
+  console.log('hi');
+}
+
+console.log(dog);
+let dog = '바둑이'
