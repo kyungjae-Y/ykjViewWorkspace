a7b566fbc6f5cb89bc441ab1c5864b9ee4d2259a
kyungjae-Y <yoonkj2008@gmail.com>
2024-01-11 15:07:01 +0900

    Update Ex08_hoisting.js

--- javascript/Ex08_hoisting.js
+++ javascript/Ex08_hoisting.js
@@ -15,3 +15,19 @@ function sayHi() {
 
 console.log(dog);
 let dog = '바둑이'
+
+// let, const VS var
+// var -> 함수스코프 영역, 변수 선언전에 출력 가능 : undefined 출력
+// let, const -> 블록스코프 영역, 변수 선언전에 출력 불가능 : 에러 출력
+
+var i = 99;
+for (var i = 1; i < 10; i++) {
+
+}
+console.log(i);
+
+let i = 99;
+for (let i = 1; i < 10; i++) {
+
+}
+console.log(i);
