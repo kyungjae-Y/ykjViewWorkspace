1c4559e961af10c39da7da96269062e29a58b5f4
kyungjae-Y <yoonkj2008@gmail.com>
2024-01-12 10:11:39 +0900

    Update Ex15_arrayfuncQuiz.js

--- javascript/Ex15_arrayfuncQuiz.js
+++ javascript/Ex15_arrayfuncQuiz.js
@@ -20,12 +20,12 @@ pets.forEach(obj => console.log(obj))
 
 // 동물의 kind 가 개 인것을 찾으시오
 console.log('문제 1');
-let result = pets.find(obj => obj.kind == '개')
+let result = pets.find(obj => obj.kind === '개')
 console.log(result);
 
 // 동물의 kind 가 고양이가 아닌것만 빼서 배열로 만드시오
 console.log('문제 2');
-result = pets.filter(obj => obj.kind != '고양이')
+result = pets.filter(obj => obj.kind !== '고양이')
 console.log(result);
 // 총 동물의 평균 나이를 구하시오
 console.log('문제 3');
@@ -34,8 +34,8 @@ console.log(result);
 // 동물 나이순을 내림차순 정렬
 console.log('문제 4');
 let copypets = [...pets]
-result = pets.map(a => a);
-// result = copypets.sort((a, b) => b.age - a.age)
+// result = pets.map(a => a);
+result = copypets.sort((a, b) => b.age - a.age)
 result.sort();
 result.reverse;
 console.log(result);
