c6b30309f91283d83d5590ed4ad66be6c2254fa0
kyungjae-Y <yoonkj2008@gmail.com>
2024-01-12 17:29:31 +0900

    Create Ex28.js

---
+++ javascript/Ex28.js
@@ -0,0 +1,13 @@
+  let num1 = 10;
+  let num2 = 20;
+
+  function add(x, y) {
+    let result = x + y;
+    return result
+  }
+  let multiply = function (x, y) {
+    let result = x * y;
+    return result
+  }
+  let res1 = add(num1, num2);
+  let res2 = multiply(num1, num2);
