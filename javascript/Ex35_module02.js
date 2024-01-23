08e14e568fbde442f97aca609d8f0b9e9f0449f1
kyungjae-Y <yoonkj2008@gmail.com>
2024-01-23 10:39:36 +0900

    Create Ex35_module02.js

---
+++ javascript/Ex35_module02.js
@@ -0,0 +1,10 @@
+import * as counter from "./Ex36.counter.js";
+
+counter.increase();
+counter.increase();
+counter.increase();
+counter.increase();
+
+console.log(counter.getCount());
+
+console.log(counter.count);
